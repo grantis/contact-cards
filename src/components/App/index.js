ffd75e73b9fa96a681e52280e75b7f8f5b817776
Grantis <grantrigby1992@gmail.com>
2019-06-28 13:10:20 +1200

    refactor

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
 import LandingPage from '../Landing';
-import SignUpPage from '../SignUp';
-import SignInPage from '../SignIn';
 import HomePage from '../Home';
 import AccountPage from '../Account';
 
@@ -16,14 +14,9 @@ const App = () => (
     <div>
       <Navigation />
       <Route path={ROUTES.HOME} component={HomePage} />
-      {/* <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      <Route path={ROUTES.SIGN_IN} component={SignInPage} /> */}
-      {/* <Route
-        path={ROUTES.PASSWORD_FORGET}
-        component={PasswordForgetPage}
-      /> */}
+      
       <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
-      {/* <Route exact path={ROUTES.LANDING} component={LandingPage} /> */}
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
     </div>
   </Router>
 );
